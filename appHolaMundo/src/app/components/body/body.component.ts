719ae0f902c813a0d66525f2d4d84705108fa15a
SniperMonkey <93905119+monkeitrick@users.noreply.github.com>
2022-11-29 12:37:11 +0100

    appHolaMundo terminada

--- appHolaMundo/src/app/components/body/body.component.ts
+++ appHolaMundo/src/app/components/body/body.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent {
+  
+  personajes:string[]=['Spiderman','Hulk','Thor','Dardevil'];
 
+  mostrar= true;
+  
+  frase: any={
+    mensaje:'Un gran poder requiere una gran responsabilidad',
+    autor:'Ben Parker'
+  };
 }
